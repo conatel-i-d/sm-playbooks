34ef76353e13fdaec6b9c1aa84a5257c14860163
ibarretorey <ibarretorey@gmail.com>
2020-03-20 06:58:06 +0000

    repair test_get_inventory

--- inventory/test_get_inventory.py
+++ inventory/test_get_inventory.py
@@ -1,7 +1,50 @@
 #!/usr/bin/env python
 
+import os
+import sys
+import argparse
 from ansible.module_utils import urls
-import json
-result = urls.open_url("https://api/api/switch/inventory")
-inventory = json.load(result.read())["item"]
-print(json.dumps(inventory))
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+class ExampleInventory(object):
+
+    def __init__(self):
+        self.inventory = {}
+        self.read_cli_args()
+
+        # Called with `--list`.
+        if self.args.list:
+            self.inventory = self.example_inventory()
+        # Called with `--host [hostname]`.
+        elif self.args.host:
+            # Not implemented, since we return _meta info `--list`.
+            self.inventory = self.empty_inventory()
+        # If no groups or vars are present, return empty inventory.
+        else:
+            self.inventory = self.empty_inventory()
+
+        print json.dumps(self.inventory);
+
+    # Example inventory for testing.
+    def example_inventory(self):
+        res = urls.open_url("http://api/api/switch/inventory")
+        res_data = json.loads(res.read())
+        sw_inv = res_data["item"]
+        return sw_inv
+
+    # Empty inventory for testing.
+    def empty_inventory(self):
+        return {'_meta': {'hostvars': {}}}
+
+    # Read the command line args passed to the script.
+    def read_cli_args(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--list', action = 'store_true')
+        parser.add_argument('--host', action = 'store')
+        self.args = parser.parse_args()
+
+# Get the inventory.
+ExampleInventory()
